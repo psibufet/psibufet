35ae10a716ecf3ce4406fea70d2d4d78a403c4e5
codestickdev <35964064+codestickdev@users.noreply.github.com>
2021-06-11 21:07:27 +0200

    LP Sale - referral code fix

--- js/referralCode_lp.js
+++ js/referralCode_lp.js
@@ -10,56 +10,39 @@ $(document).ready(function(){
 		}
 	}  
 
-	var currentCode = window.location.pathname.replace('/lp/', '');
-    var redirect = window.location.href.replace('/lp/' + currentCode + '', '/lp');
-    console.log(currentCode);
-
-    if(currentCode !== '/lp'){
+	var currentCode = GetURLParameter('code');
+    if(typeof currentCode !== 'undefined'){
         $.ajax({
             url:'https://app.psibufet.pl/api/order/couponcode/' + currentCode,
             error: function(){
                 console.log('Error while code loading :(');
-                window.location.replace(redirect);
             },
             success: function(){
                 $.getJSON("https://app.psibufet.pl/api/order/couponcode/" + currentCode, function (data) {
-                    if (data.purpose == "CLIENT"){
-                        window.location.replace(redirect + "?code=" + currentCode + '&utm_source=MGM&utm_medium=referral_link&utm_campaign=' + currentCode + '&amount=' + data.amount + '&type=' + data.type);
-                    }else if (data.purpose == "PARTNER"){
-                        window.location.replace(redirect + "?code=" + currentCode + '&utm_source=partner&utm_medium=referral_link&utm_campaign=' + currentCode + '&amount=' + data.amount + '&type=' + data.type);
-                    }else if (data.purpose == "MARKETING"){
-                        window.location.replace(redirect + "?code=" + currentCode + '&amount=' + data.amount + '&type=' + data.type);
-                    }else if (data.purpose == "INFLUENCER"){
-                        window.location.replace(redirect + "?code=" + currentCode + '&amount=' + data.amount + '&type=' + data.type);
-                    }	
+                    var amount = data.amount;
+                    var type = data.type;
+                    var firstletter = currentCode.charAt(0);
+
+                    var saleBar = $('.salePromobar').find('p')
+                    if(type == 'PERCENT' && firstletter == '2'){
+                        saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type">%</span></b>na dwie pierwsze dostawy');
+                    }else if (type == 'PERCENT' && firstletter !== '2'){
+                        saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type">%</span></b>na Twoje zamówienie');
+                    }else if (type == 'AMOUNT' && firstletter == '2'){
+                        saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type"> PLN</span></b>na dwie pierwsze dostawy');
+                    }else if (type == 'AMOUNT' && firstletter !== '2'){
+                        saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type"> PLN</span></b>na Twoje zamówienie');
+                    }
+                    $(document).ready(function(){
+                        $('.salePromobar').addClass('salePromobar--toggle');
+                        setTimeout(function(){
+                            $('.salePromobar').slideDown();
+                        }, 200);
+
+                        $('.stickyBtn').addClass('stickyBtn--toggle');
+                    });
                 });
             }
-        });
-    }
-
-    var code = GetURLParameter('code');
-    var amount = GetURLParameter('amount');
-    var type = GetURLParameter('type');
-    var firstletter = code.charAt(0);
-
-    if(typeof code !== 'undefined'){
-        var saleBar = $('.salePromobar').find('p')
-        if(type == 'PERCENT' && firstletter == '2'){
-            saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type">%</span></b>na dwie pierwsze dostawy');
-        }else if (type == 'PERCENT' && firstletter !== '2'){
-            saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type">%</span></b>na Twoje zamówienie');
-        }else if (type == 'AMOUNT' && firstletter == '2'){
-            saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type"> PLN</span></b>na dwie pierwsze dostawy');
-        }else if (type == 'AMOUNT' && firstletter !== '2'){
-            saleBar.html('<b>-<span class="amount">' + amount + '</span><span class="type"> PLN</span></b>na Twoje zamówienie');
-        }
-        $(document).ready(function(){
-            $('.salePromobar').addClass('salePromobar--toggle');
-            setTimeout(function(){
-                $('.salePromobar').slideDown();
-            }, 200);
-
-            $('.stickyBtn').addClass('stickyBtn--toggle');
         });
     }
 });
