fa6295970bdd058034608f0c62abf5845e9e37ab
codestickdev <35964064+codestickdev@users.noreply.github.com>
2021-10-08 14:45:11 +0200

    Book - changes

--- page-templates/page_ksiazka.php
+++ page-templates/page_ksiazka.php
@@ -11,119 +11,68 @@ get_header(); ?>
     </div>
 </section>
 
-<section class="book__content">
-    <div class="container">
-        <div class="pbShortcuts book__content__header">
-            <h2 class="getMarker">najlepsia <span class="marker">książka</span> o jedzeniu</h2>
-            <p>Jedyna taka psiążka kucharska. <span class="bold">63 przepisy</span> na przepyszne, zdrowe i pełnowartościowe posiłki dla każdego
-                psa.</p>
-        </div>
-        <div class="book__price">
-            <p class="book__price__cost">
-                <img src="/wp-content/themes/psibufet/images/chevron_down.svg"/>
-                CENA KSIĄŻKI W KSIĘGARNI: 59<span>99</span>ZŁ
-            </p>
-            <p class="book__price__free">
-                <img src="/wp-content/themes/psibufet/images/chevron_down.svg"/>
-                CENA PRZY ZAKUPIE KARMY: <span>ZA DARMO!</span>
-            </p>
-        </div>
-        <div>
-            img
-        </div>
-        <a href="#" class="btn freeBook"><span>Odbierz za darmo</span></a>
-        <a href="#" class="buyBook">Nie chcę prezentu. Pokaż, gdzie kupić tylko książkę.</a>
-        <div class="book__description">
-            <p>
-                <span class="inner">
-                    <span class="bold">Twój pies uwielbia ruch i długie zabawy?</span> Zapewnij mu odpowiednią dawkę energii i ugotuj
-                    wysokokaloryczne
-                    danie z wołowiny z dodatkiem sardynek.
-                </span>
-                <span class="inner">
-                    <span class="bold">Psiemu seniorowi</span> na deser zaproponuj galaretkę z kurzych łapek – smakuje wyśmienicie i doskonale
-                    wspomaga stawy.
-                </span>
-                <span class="inner">
-                    A jeśli twój pupil ma <span class="bold">objawy alergii pokarmowej,</span> wypróbuj połączenie banana i mięsa z królika, które z
-                    powodzeniem zastąpi tradycyjną karmę.
-                </span>
-                <span class="inner">
-                    <span class="bold">Gotowanie dla psa wcale nie musi być trudne!</span> W tej książce znajdziesz proste pomysły na <span class="bold">PSOTRAWY,</span>
-                    czyli
-                    dania główne, starannie opracowane tak, aby każdego dnia twój czworonożny przyjaciel otrzymywał to,
-                    czego najbardziej potrzebuje.
-                </span>
-                <span class="inner">Dzięki <span class="bold">PSIESMAKOM</span> wzbogacisz dietę swojego pupila zdrowymi, w pełni naturalnymi
-                    przekąskami.
-                </span>
-                <span class="inner">A na <span class="bold">specjalne okazje</span> – urodziny, adopciny oraz inne święta – przygotowaliśmy kilka naprawdę
-                    wyjątkowych <span class="bold">PSIECJAŁÓW!</span>
-                </span>
-            </p>
-        </div>
+<section class="bookContent book__content container">
+    <div class="book__content__header">
+        <h2 class="getMarker">najlepsia <span class="marker">książka</span> o jedzeniu</h2>
+        <p>Jedyna taka psiążka kucharska. <span class="bold">63 przepisy</span> na przepyszne, zdrowe i pełnowartościowe posiłki dla każdego
+            psa.</p>
+    </div>
+    <div class="book__price">
+        <p class="book__price__cost">
+            <img src="/wp-content/themes/psibufet/images/chevron_down.svg"/>
+            CENA KSIĄŻKI W KSIĘGARNI: 59<span>99</span>ZŁ
+        </p>
+        <p class="book__price__free">
+            <img src="/wp-content/themes/psibufet/images/chevron_down.svg"/>
+            CENA PRZY ZAKUPIE KARMY: <span>ZA DARMO!</span>
+        </p>
+    </div>
+    <div class="bookContent__image">
+        <img src="<?php echo get_template_directory_uri() . '/images/book/book_img.png' ?>"/>
+        <img class="gratis" src="<?php echo get_template_directory_uri() . '/images/book/gratis_ico.svg' ?>"/>
+    </div>
+    <a href="#" class="btn freeBook"><span>Odbierz za darmo</span></a>
+    <a href="#" class="buyBook">Nie chcę prezentu. Pokaż, gdzie kupić tylko książkę.</a>
+    <div class="book__description">
+        <p><b>Twój pies uwielbia ruch i długie zabawy?</b> Zapewnij mu odpowiednią dawkę energii i ugotuj wysokokaloryczne danie z wołowiny z dodatkiem sardynek.</p>
+        <p><b>Psiemu seniorowi</b> na deser zaproponuj galaretkę z kurzych łapek – smakuje wyśmienicie i doskonale wspomaga stawy.</p>
+        <p>A jeśli twój pupil ma <b>objawy alergii pokarmowej,</b> wypróbuj połączenie banana i mięsa z królika, które z powodzeniem zastąpi tradycyjną karmę.</p>
+        <p><b>Gotowanie dla psa wcale nie musi być trudne!</b> W tej książce znajdziesz proste pomysły na <b>PSOTRAWY</b>, czyli dania główne, starannie opracowane tak, aby każdego dnia twój czworonożny przyjaciel otrzymywał to, czego najbardziej potrzebuje.</p>
+        <p>Dzięki <b>PSIESMAKOM</b> wzbogacisz dietę swojego pupila zdrowymi, w pełni naturalnymi przekąskami.</p>
+        <p>A na <b>specjalne okazje</b> – urodziny, adopciny oraz inne święta – przygotowaliśmy kilka naprawdę wyjątkowych <b>PSIECJAŁÓW!</b></p>
     </div>
 </section>
 
 <section class="meetMakers pbShortcuts">
-    <h2 class="getMarker">POZNAJ <span class="marker">TWÓRCÓW</span></h2>
-    <div class="makers desktopMakers">
-        <div class="box">
-            <div class="box__title">
-                <h3>dr inż. Olga Lasek</h3>
+    <div class="meetMakers__wrap">
+        <h2 class="getMarker">Poznaj <span class="marker">twórców</span></h2>
+        <div class="meetMakers__images">
+            <div class="image">
+                <img src="<?php echo get_template_directory_uri() . '/images/book/autorzy_razem.png'; ?>"/>
             </div>
-            <div class="box__desc">
-                <p>Psi dietetyk, pracownik akademicki i autorka przepisów PsiBufet.</p>
+            <div class="image">
+                <img src="<?php echo get_template_directory_uri() . '/images/book/autorzy_razem.png'; ?>"/>
             </div>
-        </div>
-        <div class="box">
-            <div class="box__title">
-                <h3>Piotr Wawrysiuk</h3>
-            </div>
-            <div class="box__desc">
-                <p>Założyciel firmy PsiBufet, na co dzień zapewniającej karmę kilku tysiącom czworonogów.</p>
+            <div class="image">
+                <img src="<?php echo get_template_directory_uri() . '/images/book/autorzy_razem.png'; ?>"/>
             </div>
         </div>
-        <div class="box">
-            <div class="box__title">
-                <h3>Aleksandra Więcławska</h3>
+        <img src="<?php echo get_template_directory_uri() . '/images/book/autorzy_razem.png'; ?>" />
+        <div class="meetMakers__content container">
+            <div class="box">
+                <h3 class="box__title">dr inż. Olga Lasek</h3>
+                <p class="box__desc">Psi dietetyk, pracownik akademicki i autorka przepisów PsiBufet.</p>
             </div>
-            <div class="box__desc">
-                <p>Redaktora serwisu Psy.pl. największego portalu w polskim “psinternecie"</p>
+            <div class="box">
+                <h3 class="box__title">Piotr Wawrysiuk</h3>
+                <p class="box__desc">Założyciel firmy PsiBufet, na co dzień zapewniającej karmę kilku tysiącom czworonogów.</p>
             </div>
-        </div>
-    </div>
-    <div class="makers mobileSlider">
-        <div class="box">
-            <div class="box__title">
-                <h3>dr inż. Olga Lasek</h3>
-            </div>
-            <div class="box__desc">
-                <p>Psi dietetyk, pracownik akademicki i autorka przepisów PsiBufet.</p>
-            </div>
-        </div>
-        <div class="box">
-            <div class="box__title">
-                <h3>Piotr Wawrysiuk</h3>
-            </div>
-            <div class="box__desc">
-                <p>Założyciel firmy PsiBufet, na co dzień zapewniającej karmę kilku tysiącom czworonogów.</p>
-            </div>
-        </div>
-        <div class="box">
-            <div class="box__title">
-                <h3>Aleksandra Więcławska</h3>
-            </div>
-            <div class="box__desc">
-                <p>Redaktora serwisu Psy.pl. największego portalu w polskim “psinternecie"</p>
+            <div class="box">
+                <h3 class="box__title">Aleksandra Więcławska</h3>
+                <p class="box__desc">Redaktora serwisu Psy.pl. największego portalu w polskim “psinternecie"</p>
             </div>
         </div>
     </div>
-    <div class="bookPagination">
-            <div class="bookPrev"></div>
-            <div class="bookNext"></div>
-            <div class="bookDots"></div>
-        </div>
 </section>
 
 <section class="book__library">
@@ -131,13 +80,13 @@ get_header(); ?>
         <h2>KSIĄŻKA DOSTĘPNA W KSIĘGARNIACH</h2>
         <div class="book__box">
             <div class="book__box__img">
-                <img src="/wp-content/themes/psibufet/images/book/bonito_logo.png" />
+                <img src="/wp-content/themes/psibufet/images/book/bonito_logo.svg" />
             </div>
             <div class="book__box__img">
-                <img src="/wp-content/themes/psibufet/images/book/empik_logo.png" />
+                <img src="/wp-content/themes/psibufet/images/book/empik_logo.svg" />
             </div>
             <div class="book__box__img">
-                <img src="/wp-content/themes/psibufet/images/book/znak_logo.png" />
+                <img src="/wp-content/themes/psibufet/images/book/znak_logo.svg" />
             </div>
         </div>
     </div>
