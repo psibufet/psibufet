9897825d9c446274fd15099955bdadcdfae219fe
codestickdev <piotrdevv@gmail.com>
2021-10-14 16:19:36 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -634,7 +634,7 @@ function availableForm(){
 	$mail = isset( $_POST['mail'] ) ? $_POST['mail'] : '';
 	$phone = isset( $_POST['phone'] ) ? $_POST['phone'] : '';
 	
-	$to = 'michalina@psibufet.pl';
+	$to = 'piotrdevv@gmail.com';
 	$subject = '[PsiBufet] Zgłoszenie ambasadora';
 	$message = "Typ: " . $type . "\r\nFirma: " . $company . "\r\nMail: " . $mail . "\r\nPhone: " . $phone;
 
