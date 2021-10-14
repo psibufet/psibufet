4bfb33fbe40a9e27e9ab659c199b9b35503b8c02
codestickdev <piotrdevv@gmail.com>
2021-10-14 16:07:32 +0200

    Partner mail response - beta 1.1v

--- email-templates/partner-template.php
+++ email-templates/partner-template.php
@@ -1,154 +1,163 @@
-<style>
-    @font-face {
-        font-family: "Bebas Neue";
-        src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/BebasNeue/BebasNeue-Regular.ttf') format("truetype");
-    }
-    @font-face {
-        font-family: "Roboto";
-        font-weight: 400;
-        src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/Roboto/Roboto-Regular.ttf') format("truetype");
-    }
-    @font-face {
-        font-family: "Roboto";
-        font-weight: 500;
-        src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/Roboto/Roboto-Medium.ttf') format("truetype");
-    }
-    @font-face {
-        font-family: "Roboto";
-        font-weight: 700;
-        src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/Roboto/Roboto-Bold.ttf') format("truetype");
-    }
-    .emailWrap{
-        background-color: #E8E8E8;
-        padding: 20px 0;
-    }
-    .email{
-        background-color: #fff;
-        border: 1px solid rgba(0, 0, 0, 0.2);
-        box-sizing: border-box;
-        box-shadow: 4px 4px 0px rgba(0, 0, 0, 0.1);
-        max-width: 600px;
-        width: 100%;
-        margin: 0 auto;
-        padding: 30px 26px;
-    }
-    .email .logo{
-        display: block;
-        margin: 0 auto 20px;
-    }
-    .email p{
-        font-size: 14px;
-        line-height: 18px;
-        color: #000;
-        font-family: 'Roboto';
-        margin: 0 0 20px;
-    }
-    .email p.beforeul{
-        margin: 0 0 5px;
-    }
-    .email p.center{
-        text-align: center;
-    }
-    .email ul{
-        margin: 0 0 20px;
-        padding: 0 0 0 25px;
-    }
-    .email ul.decimal{
-        list-style: decimal;
-    }
-    .email ul li{
-        font-size: 14px;
-        line-height: 18px;
-        color: #000;
-        font-family: 'Roboto';
-        margin: 0 0 3px;
-    }
-    .email ul li:last-of-type{
-        margin: 0;
-    }
-    .btn{
-        display: flex;
-        max-width: fit-content;
-        max-width: -moz-max-content;
-        max-width: -webkit-fit-content;
-        min-width: 225px;
-        margin: 0 auto 20px;
-        align-items: center;
-        justify-content: center;
-        background-color: #f15748;
-        height: 52px;
-        box-shadow: 4px 4px 0px rgba(0, 0, 0, 0.1);
-        text-decoration: none;
-        transition: ease .3s;
-    }
-    .btn:hover{
-        opacity: .7;
-    }
-    .btn span{
-        font-size: 20px;
-        font-weight: 500;
-        line-height: 1;
-        font-family: 'Roboto';
-        text-decoration: none;
-        color: #fff;
-        text-align: center;
-        padding: 0 20px;
-    }
-    .email .image{
-        text-align: center;
-    }
-    .email .image img{
-        max-width: 200px;
-        margin: 0 auto 15px;
-    }
-    .email .image p{
-        font-size: 16px;
-        line-height: 19px;
-        color: #000;
-        text-align: center;
-        margin: 0;
-    }
-    .email .image p.big{
-        font-size: 20px;
-        line-height: 24px;
-        font-weight: 400;
-        font-family: 'Bebas Neue', sans-serif;
-        color: #000;
-        text-align: center;
-        margin: 0;
-    }
-</style>
-<div class="emailWrap">
-    <div class="email">
-        <img class="logo" src="https://psibufet.pl/wp-content/themes/psibufet/images/lpsale/psibufet_logo_lp.svg"/>
-        <p>Cześć!<br/>Nazywam się Michalina i przeprowadzę Cię przez proces dołączania do naszej ekipy 🐶</p>
-        <p>Jest mi bardzo miło, że się ze mną kontaktujesz 😊 Wysyłając do nas zgłoszenie, pokazujesz, że zależy Ci na szczęściu i zdrowiu psiaków, tak jak nam wszystkim w PsiBufet.</p>
-        <p>Program Partnerski daje Tobie wiele przywilejów i możliwości, dzięki którym możesz jeszcze łatwiej pomagać psiakom w całej Polsce.</p>
-        <p class="beforeul">Jako Partner PsiBufet zyskujesz:</p>
-        <ul>
-            <li>indywidualny kod zniżkowy na dietę PsiBufet w wysokości -30% do rozpowszechniania,</li>
-            <li>wynagrodzenie 100 zł za każdą pierwszą oraz 50 zł za każdą drugą sprzedaż z kodu zniżkowego,</li>
-            <li>rozliczenie na podstawie podpisanej umowy partnerskiej,</li>
-            <li>materiały edukacyjne i reklamowe o PsiBufet,</li>
-            <li>możliwość stania się częścią firmy, gdzie każdy dzień rozpoczynamy od głaskania piesków 🐕</li>
-        </ul>
-        <p>Decydujesz się do nas dołączyć? To świetnie! ❤️</p>
-        <p class="beforeul">Co teraz?</p>
-        <ul class="decimal">
-            <li>Sporządzamy umowę na podstawie podanych mi przez Ciebie danych i przesyłam do Ciebie uzupełnioną umowę poprzez platformę Autenti do podpisu.</li>
-            <li>Przesyłam do Ciebie wszelkie informacje potrzebne do promowania diety PsiBufet z Twoim kodem zniżkowym.</li>
-            <li>Otrzymujesz od nas wynagrodzenie oraz dostęp do specjalnych materiałów i wydarzeń.</li>
-            <li>Jesteś częścią naszej ekipy PsiBufet i wspólnie z nami zmieniasz oblicze żywienia psów w Polsce!</li>
-        </ul>
-        <p class="center">To co, zaczynamy? 😊</p>
-        <p class="center">Pod poniższym linkiem znajdziesz umowę do wglądu.</p>
-        <a href="https://psibufet.pl/umowa_partnerska_psibufet.pdf" class="btn"><span>Umowa partnerska</span></a>
-        <p class="center">Czekam na Twoją decyzję.<br/>Z psiozdrowieniami,</p>
-        <div class="image">
-            <img src="https://psibufet.pl/wp-content/themes/psibufet/images/email/michalina_photo.png"/>
-            <p>Michalina</p>
-            <p class="big">Specjalistka ds. partnerstw</p>
+<!DOCTYPE html>
+    <head>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <style>
+            @font-face {
+                font-family: "Roboto";
+                font-weight: 400;
+                src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/Roboto/Roboto-Regular.ttf') format("truetype");
+            }
+            @font-face {
+                font-family: "Roboto";
+                font-weight: 500;
+                src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/Roboto/Roboto-Medium.ttf') format("truetype");
+            }
+            @font-face {
+                font-family: "Roboto";
+                font-weight: 700;
+                src: url('https://psibufet.pl/wp-content/themes/psibufet/fonts/Roboto/Roboto-Bold.ttf') format("truetype");
+            }
+            .emailWrap{
+                background-color: #E8E8E8;
+                padding: 20px 0;
+            }
+            .email{
+                background-color: #fff;
+                border: 1px solid rgba(0, 0, 0, 0.2);
+                box-sizing: border-box;
+                box-shadow: 4px 4px 0px rgba(0, 0, 0, 0.1);
+                max-width: 600px;
+                width: 100%;
+                margin: 0 auto;
+                padding: 30px 26px;
+            }
+            .email .logo{
+                display: block;
+                max-width: 180px;
+                width: 100%;
+                height: 30px;
+                background-image: url('http://psibufet.local/wp-content/themes/psibufet/images/email/psibufet_logo.svg');
+                background-repeat: no-repeat;
+                background-size: contain;
+                margin: 0 auto 20px;
+            }
+            .email p{
+                font-size: 14px;
+                line-height: 18px;
+                color: #000;
+                font-family: 'Roboto';
+                margin: 0 0 20px;
+            }
+            .email p.beforeul{
+                margin: 0 0 5px;
+            }
+            .email p.center{
+                text-align: center;
+            }
+            .email ul{
+                margin: 0 0 20px;
+                padding: 0 0 0 25px;
+            }
+            .email ul.decimal{
+                list-style: decimal;
+            }
+            .email ul li{
+                font-size: 14px;
+                line-height: 18px;
+                color: #000;
+                font-family: 'Roboto';
+                margin: 0 0 3px;
+            }
+            .email ul li:last-of-type{
+                margin: 0;
+            }
+            .btn{
+                display: flex;
+                max-width: fit-content;
+                max-width: -moz-max-content;
+                max-width: -webkit-fit-content;
+                min-width: 225px;
+                margin: 0 auto 20px;
+                align-items: center;
+                justify-content: center;
+                background-color: #f15748;
+                height: 52px;
+                box-shadow: 4px 4px 0px rgba(0, 0, 0, 0.1);
+                text-decoration: none;
+                transition: ease .3s;
+            }
+            .btn:hover{
+                opacity: .7;
+            }
+            .btn span{
+                width: 100%;
+                font-size: 20px;
+                font-weight: 500;
+                line-height: 52px;
+                font-family: 'Roboto';
+                text-decoration: none;
+                color: #fff;
+                text-align: center;
+                padding: 0 20px;
+            }
+            .email .image{
+                text-align: center;
+            }
+            .email .image img{
+                max-width: 200px;
+                margin: 0 auto 15px;
+            }
+            .email .image p{
+                font-size: 16px;
+                line-height: 19px;
+                font-family: 'Roboto', sans-serif;
+                color: #000;
+                text-align: center;
+                margin: 0;
+            }
+            .email .image p.big{
+                font-size: 20px;
+                line-height: 24px;
+                font-weight: 500;
+                color: #000;
+                text-align: center;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="emailWrap">
+            <div class="email">
+                <div class="logo"></div>
+                <p>Cześć!<br/>Nazywam się Michalina i przeprowadzę Cię przez proces dołączania do naszej ekipy 🐶</p>
+                <p>Jest mi bardzo miło, że się ze mną kontaktujesz 😊 Wysyłając do nas zgłoszenie, pokazujesz, że zależy Ci na szczęściu i zdrowiu psiaków, tak jak nam wszystkim w PsiBufet.</p>
+                <p>Program Partnerski daje Tobie wiele przywilejów i możliwości, dzięki którym możesz jeszcze łatwiej pomagać psiakom w całej Polsce.</p>
+                <p class="beforeul">Jako Partner PsiBufet zyskujesz:</p>
+                <ul>
+                    <li>indywidualny kod zniżkowy na dietę PsiBufet w wysokości -30% do rozpowszechniania,</li>
+                    <li>wynagrodzenie 100 zł za każdą pierwszą oraz 50 zł za każdą drugą sprzedaż z kodu zniżkowego,</li>
+                    <li>rozliczenie na podstawie podpisanej umowy partnerskiej,</li>
+                    <li>materiały edukacyjne i reklamowe o PsiBufet,</li>
+                    <li>możliwość stania się częścią firmy, gdzie każdy dzień rozpoczynamy od głaskania piesków 🐕</li>
+                </ul>
+                <p>Decydujesz się do nas dołączyć? To świetnie! ❤️</p>
+                <p class="beforeul">Co teraz?</p>
+                <ul class="decimal">
+                    <li>Sporządzamy umowę na podstawie podanych mi przez Ciebie danych i przesyłam do Ciebie uzupełnioną umowę poprzez platformę Autenti do podpisu.</li>
+                    <li>Przesyłam do Ciebie wszelkie informacje potrzebne do promowania diety PsiBufet z Twoim kodem zniżkowym.</li>
+                    <li>Otrzymujesz od nas wynagrodzenie oraz dostęp do specjalnych materiałów i wydarzeń.</li>
+                    <li>Jesteś częścią naszej ekipy PsiBufet i wspólnie z nami zmieniasz oblicze żywienia psów w Polsce!</li>
+                </ul>
+                <p class="center">To co, zaczynamy? 😊</p>
+                <p class="center">Pod poniższym linkiem znajdziesz umowę do wglądu.</p>
+                <a href="https://psibufet.pl/umowa_partnerska_psibufet.pdf" class="btn"><span>Umowa partnerska</span></a>
+                <p class="center">Czekam na Twoją decyzję.<br/>Z psiozdrowieniami,</p>
+                <div class="image">
+                    <img src="https://psibufet.pl/wp-content/themes/psibufet/images/email/michalina_photo.png"/>
+                    <p>Michalina</p>
+                    <p class="big">Specjalistka ds. partnerstw</p>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-
+    </body>
+</html>
