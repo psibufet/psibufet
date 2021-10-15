c1a4ecdbf12fac3eef6cdcdec2dcc5a2e8ae0315
codestickdev <piotrdevv@gmail.com>
2021-10-15 15:31:23 +0200

    Update page_front-page.php

--- page-templates/page_front-page.php
+++ page-templates/page_front-page.php
@@ -7,7 +7,26 @@ get_header(); ?>
 
 <main class="pbpage pbpage--frontpage">
 
-    <?php include get_template_directory() . '/template-parts/_homeHeaderV2.php'; ?>
+    <?php // include get_template_directory() . '/template-parts/_homeHeaderV2.php'; ?>
+
+    <section class="homeHeaderV2" style="background-image: url('/wp-content/themes/psibufet/images/pricedrop/sgNewHeader-min.jpg');">
+        <div class="homeHeaderV2__wrap">
+            <div class="homeHeaderV2__content">
+                <h2>Świeże spojrzenie na psie jedzenie</h2>
+                <div class="tags">
+                    <div><p>Naturalne składniki</p></div>
+                    <div><p>Idealna porcja</p></div>
+                    <div><p>Dostawa pod drzwi</p></div>
+                </div>
+                <form class="homeHeaderV2__form dogNameForm" action="https://zamowienie.psibufet.pl/" autocomplete="off">
+                    <h2 class="getMarker">Tylko <span class="number">1</span> minutę zajmie stworzenie <span class="marker">idealnego</span> planu dla Twojego psa</h2>
+                    <input type="text" value="" placeholder="Wpisz imię swojego pupila" autocomplete="off"/>
+                    <button type="submit"><span>Stwórz dietę</span></button>
+                    <a href="https://zamowienie.psibufet.pl/" class="more">Mam więcej niż jednego psa.</a>
+                </form>
+            </div>
+        </div>
+    </section>
     
     <section id="page-continue" class="homeReviews">
         <?php if( have_rows('after_header_logos') ): ?>
