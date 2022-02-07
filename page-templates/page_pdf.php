1af0b58cab40763693dfaa10890f1b6024759e0e
codestickdev <piotrdevv@gmail.com>
2022-02-07 13:56:53 +0100

    Update page_pdf.php

--- page-templates/page_pdf.php
+++ page-templates/page_pdf.php
@@ -7,7 +7,7 @@ get_header(); ?>
 <main class="psibufet psibufet--pdf">
     <div class="pagePDF" data-pdf="">
         <div class="pagePDF__wrap">
-            <img src="<?php echo get_template_directory_uri() . '/images/pdf_dog.gif'; ?>"/>
+            <img src="<?php echo get_template_directory_uri() . '/images/pdf_dog.gif'; ?>" class="no-lazyload"/>
             <h1 class="status">Czekaj na pobranie pliku PDF</h1>
             <a href="<?php echo get_field('download_pdf'); ?>" id="download_pdf" download></a>
             <a href="https://psibufet.pl/" id="back" class="btn btn--back"><span>Powrót na stronę główną</span></a>
