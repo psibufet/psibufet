6ea9b20a17153f8035f19308fd952b8f3779d585
codestickdev <piotrdevv@gmail.com>
2022-03-02 11:34:04 +0100

    LP Szczeniaki fix

--- page-templates/page_sczeniaki.php
+++ page-templates/page_sczeniaki.php
@@ -30,11 +30,11 @@ get_header(); ?>
                     <?php endif; ?>
                     <?php if( $google ): ?>
                         <div class="stars">
-                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg"/>
-                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg"/>
-                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg"/>
-                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg"/>
-                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg"/>
+                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg" class="no-lazyload"/>
+                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg" class="no-lazyload"/>
+                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg" class="no-lazyload"/>
+                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg" class="no-lazyload"/>
+                            <img src="/wp-content/themes/psibufet/images/reviews/star.svg" class="no-lazyload"/>
                         </div>
                         <p><?php echo $googleRate; ?>/5 wg opinii klientów</p>
                     <?php endif; ?>
