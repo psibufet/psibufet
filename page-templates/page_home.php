bfd6edcc6cc694df261e1780937773d32319ac2c
codestickdev <piotrdevv@gmail.com>
2022-07-11 17:07:33 +0200

    main page food photo mobile

--- page-templates/page_home.php
+++ page-templates/page_home.php
@@ -80,7 +80,8 @@ get_header(); ?>
     <section class="homeFood">
         <div class="homeFood__wrap">
             <div class="homeFood__image">
-                <img src="<?php echo get_template_directory_uri() . '/images/home/homeFood_new.png'; ?>"/>
+                <img class="mobile" src="<?php echo get_template_directory_uri() . '/images/home/homeFood.png'; ?>"/>
+                <img class="desktop" src="<?php echo get_template_directory_uri() . '/images/home/homeFood_new.png'; ?>"/>
             </div>
             <div class="homeFood__content">
                 <h2 class="getMarker">Poznaj nasze <span class="marker">smaki</span></h2>
